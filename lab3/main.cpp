4e1cfd7e4aa209b1a25047143c7b38014dc0a155
Robert Fitzgerald <robfitzgerald@gmail.com>
2015-07-22 13:22:55 -0600

    YES.  correct optimal processing.

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, const char * argv[]) {
     std::vector<int> refString;
-    int refStringSrc [22] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
-    for (int i = 0; i < 22; ++i) {
+    int refStringSrc [20] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
+    for (int i = 0; i < 20; ++i) {
         refString.push_back(refStringSrc[i]);
     }
     vmManager vmManager(3, 1);
--- lab3/vmManager.cpp
+++ lab3/vmManager.cpp
@@ -38,33 +38,64 @@ void vmManager::access() {
         // page replacement
         switch(algorithm) {
             case 1: {
-                // optimal
                 std::list<int>::iterator longestPage = pagesTable.end();
                 int longestDistance = 0;
-                for (std::list<int>::iterator i = pagesTable.begin(); i != pagesTable.end(); ++i) {
-                    if (pages[time] != *i) {
-                        bool pageIsRequestedAgain = false;
-                        for (int j = time; j < pages.size(); ++j) {
-                           if (pages[j] == *i) {
-                               if ((j - time) > longestDistance) {
-                                   std::cout << pages[j] << "==" << *i << ", (j-time) > longestDistance " << (j - time) << " > " << longestDistance << std::endl;
-                                   std::cout << "j is " << j << " and time is " << time << std::endl;
-                                   longestPage = i;
-                                   longestDistance = j - time;
-                                   pageIsRequestedAgain = true;
-                               }
-                               j = (int) pages.size();  // basically, 'continue;' out of for(j) loop
+                bool findingNextOccurence = true;
+                bool findingLongestPageDistance = true;
+                for (std::list<int>::iterator i = pagesTable.begin(); ((findingLongestPageDistance && (i != pagesTable.end()))); ++i) {
+                    bool nextOccurenceExists = false;
+                    findingNextOccurence = true;
+                    for (int j = time; (findingNextOccurence && (j < pages.size())); ++j) {
+                        std::cout << "checking for " << *i << " at pages[" << j << "] and found " << pages[j] << std::endl;
+                        if (*i == pages[j]) {
+                            findingNextOccurence = false;
+                            nextOccurenceExists = true;
+                            int distance = j - time;
+                            if (distance > longestDistance) {
+                                longestDistance = distance;
+                                longestPage = i;
+                                std::cout << pages[j] << "==" << *i << ", distance > longestDistance " << distance << " > " << longestDistance << std::endl;
                             }
                         }
-                        // if it doesn't get requested again, make it the longest.  tie is broken
-                        // by page being the first not requested page in the ref string
-                        if (!pageIsRequestedAgain && (longestDistance < (int) pages.size())) {
-                            std::cout << *i << " is not referenced again" << std::endl;
-                            longestPage = i;
-                            longestDistance = (int) pages.size();
-                        }
+                    }
+                    if (!nextOccurenceExists) {
+                        // didn't find it.  it doesn't occur again, so it
+                        // can be removed.  longestDistance comparison is irrelevant.
+                        // therefore the first page to have no reoccurence is removed.2
+                        // short circuit the list.foreach() process
+                        longestPage = i;
+                        //i = pagesTable.end();
+                        std::cout << *i << " not found." << std::endl;
+                        findingLongestPageDistance = false;
                     }
                 }
+                
+                // optimal
+//                std::list<int>::iterator longestPage = pagesTable.end();
+//                int longestDistance = 0;
+//                for (std::list<int>::iterator i = pagesTable.begin(); i != pagesTable.end(); ++i) {
+//                    bool pageIsRequestedAgain = false;
+//                    for (int j = time; j < pages.size(); ++j) {
+//                       if (pages[j] == *i) {
+//                           if ((j - time) > longestDistance) {
+//                               std::cout << pages[j] << "==" << *i << ", (j-time) > longestDistance " << (j - time) << " > " << longestDistance << std::endl;
+//                               std::cout << "j is " << j << " and time is " << time << std::endl;
+//                               longestPage = i;
+//                               longestDistance = j - time;
+//                               pageIsRequestedAgain = true;
+//                           }
+//                           j = (int) pages.size();  // basically, 'continue;' out of for(j) loop
+//                        }
+//                    }
+//                    // if it doesn't get requested again, make it the longest.  tie is broken
+//                    // by page being the first not requested page in the ref string
+//                    if (!pageIsRequestedAgain && (longestDistance < (int) pages.size())) {
+//                        std::cout << *i << " is not referenced again" << std::endl;
+//                        longestPage = i;
+//                        longestDistance = (int) pages.size();
+//                    }
+//                    
+//                }
                 pagesTable.erase(longestPage);
                 break;
             }
